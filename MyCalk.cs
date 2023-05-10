528cad8a2f8a4c81126dee5cdcaf532c98fd42f9
karmaisthebitch <133087659+karmaisthebitch@users.noreply.github.com>
2023-05-10 11:20:02 +0500

    Add files via upload

---
+++ MyCalk.cs
@@ -0,0 +1,25 @@
+﻿namespace Calkulator
+{
+    public class MyCalk
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Mul(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Div(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
